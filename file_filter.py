7ca67842f8aaaa64283375f882d6a7a55e95d1f3
tkdguq728 <62496557+tkdguq728@users.noreply.github.com>
2023-12-12 17:24:57 +0900

    Update file_filter.py

--- file_filter.py
+++ file_filter.py
@@ -19,6 +19,3 @@ pattern = language_patterns.get(language.lower())
 filtered_files = [file for file in file_list if re.search(pattern, file['name'], re.IGNORECASE)]
 
 selected_file = random.choice(filtered_files)
-
-encoded_contents = selected_file['contents'].encode('utf-8')
-encoded_contents_base64 = base64.b64encode(encoded_contents).decode('utf-8')
