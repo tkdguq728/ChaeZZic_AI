e1eead9a591906059d2645f41f2e4dd1f8bd14c1
tkdguq728 <62496557+tkdguq728@users.noreply.github.com>
2023-12-13 22:41:11 +0900

    Update readme_processing.py

--- readme_processing.py
+++ readme_processing.py
@@ -1,11 +1,6 @@
 # README 파일 전처
 import torch
-from transformers import PreTrainedTokenizerFast
-from transformers import BartForConditionalGeneration
 import re
-
-tokenizer = PreTrainedTokenizerFast.from_pretrained('digit82/kobart-summarization')
-model = BartForConditionalGeneration.from_pretrained('digit82/kobart-summarization')
 
 def remove_markdown(readme_text):
     # 코드 블럭 제거
