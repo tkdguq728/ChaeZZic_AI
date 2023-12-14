b4b962b85e0de854d4d4874f6b46d828e87006ff
tkdguq728 <62496557+tkdguq728@users.noreply.github.com>
2023-12-14 16:56:45 +0900

    Update app.py

--- main/app.py
+++ main/app.py
@@ -5,46 +5,56 @@ from readme_processing import remove_markdown
 from readme_sum import summarize_readme
 from gen_code import generate_code_related_questions
 from gen_jobproj import generate_job_and_project_questions
+from flask_cors import CORS
 import random
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/generate_AI', methods=['POST'])
 def generate_Ai():
     try:
       # JSON 데이터 수신
       json_data = request.get_json()
-      
-      # 문자열 데이터 수신
-      job = request.form.get('string_data1')
-      language = request.form.get('string_data2')
-
+        
       # JSON 데이터를 리스트로 변환
       data_list = json.loads(json_data)
- 
-      # code 추출해서 랜덤 선택
-      code_list = extract_code_by_language(data_list, language)
-      selected_code_file = random.choice(code_list)
-      selected_code = selected_code_file.get("contents")
 
-      # readme 추출
-      readme = extract_readme_code(data_list)
-      readme_contents = readme.get("contents")
+      # job, portfolio 구분  
+      job, portfolio = extract_job_and_portfolio(data)
 
-      # readme 파일 전처리
-      readme_processing = remove_markdown(readme_contents)
-      readme_summarize = summarize_readme(readme_processing)
+      # 질문 저장할 리스트트
+      all_repo_questions = []
 
-      # 직무와 README 질문 생성
-      job_and_project_questions = generate_job_and_project_questions(job, readme_summarize)
+      for project in portfolio:
+        # language랑 file_list 추출
+        language = project['lang'] if 'lang' in project else ''
+        files = project.get("file", [])
 
-      # 코드 관련 질문 생성
-      code_related_questions = generate_code_related_questions(selected_code)
+        # language에 따른 코드 추출후에 random하게 선택
+        code_list = extract_code_by_language(files, language)
+        selected_code_file = random.choice(code_list)
+        selected_code = selected_code_file.get("contents")
 
-      all_questions = job_and_project_questions + code_related_questions
-      response_data = {"questions": all_questions}
+        # readme 추출
+        readme_contents = next((file_info["contents"] for file_info in files if file_info.get("name", "").lower() == "readme.md"), None)
 
-      return jsonify(response_data), 200
+        # readme 파일 전처리
+        readme_processing = remove_markdown(readme_contents)
+        readme_summarize = summarize_readme(readme_processing)
+
+        # 직무와 README 질문 생성
+        job_and_project_questions = generate_job_and_project_questions(job, readme_summarize)
+
+        # 코드 관련 질문 생성
+        code_related_questions = generate_code_related_questions(selected_code)
+    
+        # 질문 통합
+        all_questions = job_and_project_questions + code_related_questions
+
+        all_repo_questions.append(all_questions)
+
+      return jsonify(all_repo_questions), 200
     except Exception as e:
       return jsonify({"message": "에러가 발생했습니다."}), 500
 
