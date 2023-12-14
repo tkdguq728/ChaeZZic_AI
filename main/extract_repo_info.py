72772a86954cc8315db62446cb4033f1cde23fde
tkdguq728 <62496557+tkdguq728@users.noreply.github.com>
2023-12-14 18:28:46 +0900

    Update extract_repo_info.py

--- main/extract_repo_info.py
+++ main/extract_repo_info.py
@@ -1,5 +1,14 @@
-def extract_repo_info(project):
-    reponame = project.get("repoName")
-    language = project.get("language")
-    files = project.get("files", [])
-    return reponame, lang, files
+def extract_job_and_portfolio(data):
+    job = data.get("job")
+    portfolio = data.get("portfolio", [])
+    return job, portfolio
+
+def extract_repo_info(projects):
+    repo_info_list = []
+    for project in projects:
+        reponame = project.get("reponame")
+        lang = project.get("lang")
+        files = project.get("file", [])
+        repo_info = {"reponame": reponame, "lang": lang, "files": files}
+        repo_info_list.append(repo_info)
+    return repo_info_list
