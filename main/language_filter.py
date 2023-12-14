078da4edefd51fcf7cb7484fe53ee6c4c4f40f92
tkdguq728 <62496557+tkdguq728@users.noreply.github.com>
2023-12-14 15:25:39 +0900

    Update language_filter.py

--- main/language_filter.py
+++ main/language_filter.py
@@ -3,6 +3,6 @@ def extract_language(code_list):
     language_list = []
     for code in code_list:
         if "language" in code["name"].lower():
-            readme_code_list.append({"name": code["name"], "contents": code["contents"]})
+            language_list.append({"name": code["name"], "contents": code["contents"]})
     
-    return readme_code_list
+    return language_list
