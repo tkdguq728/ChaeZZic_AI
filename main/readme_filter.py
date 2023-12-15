a3f3dcd5dab1ec4f980947fa63b813d5ec392169
tkdguq728 <62496557+tkdguq728@users.noreply.github.com>
2023-12-15 14:08:29 +0900

    Update readme_filter.py

--- main/readme_filter.py
+++ main/readme_filter.py
@@ -3,6 +3,6 @@ def extract_readme_code(code_list):
     readme_code_list = []
     for code in code_list:
         if "readme" in code["name"].lower():
-            readme_code_list.append({"name": code["name"], "contents": code["contents"]})
+            readme_code_list.append({"name": code["name"], "content": code["content"]})
     
     return readme_code_list
